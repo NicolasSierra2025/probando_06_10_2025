50ede9fc8156087d70a41e4d87d3b92d94af220d
NicolasSierra2025 <nicolasgabrielsierra2020@gmail.com>
2025-10-20 21:16:12 -0300

    probando git arrastrando carpeta

---
+++ PARTE2_06_10_2025/PARTE2_06_10_2025/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PARTE2_06_10_2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
